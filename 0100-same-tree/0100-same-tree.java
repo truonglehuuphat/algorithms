5f9bc2feb44c178f629c859c8a45d7be32645cb3
PhatTruong <phattruongspkt@gmail.com>
2026-05-11 15:21:54 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,7 +18,7 @@ class Solution {
         if(p == null && q == null){
             return true;
         }
-        if(p == null && q != null || p != null && q == null ){
+        if(p == null || q == null ){
             return false;
         }
         if(p.val != q.val){
