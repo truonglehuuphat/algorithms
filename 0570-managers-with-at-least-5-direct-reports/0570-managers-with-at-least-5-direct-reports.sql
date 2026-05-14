5235fe0a1629a97f266fa2db4c0a48a3063170e9
PhatTruong <phattruongspkt@gmail.com>
2026-05-14 15:55:18 +0700

    Time: 375 ms (25.88%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -2,14 +2,21 @@
 -- SELECT DISTINCT e1.name as name FROM Employee e1
 -- JOIN Employee e2
 -- ON e1.id = e2.managerId
-WITH CTE AS (
-    SELECT e1.id as id, COUNT(e1.id) as count FROM Employee e1
-    JOIN Employee e2
-    ON e1.id = e2.managerId
-    GROUP BY e1.id
-    HAVING COUNT(e1.id) >= 5
-)
+-- WITH CTE AS (
+--     SELECT e1.id as id, COUNT(e1.id) as count FROM Employee e1
+--     JOIN Employee e2
+--     ON e1.id = e2.managerId
+--     GROUP BY e1.id
+--     HAVING COUNT(e1.id) >= 5
+-- )
 
-SELECT name FROM Employee
-JOIN CTE
-ON Employee.id = CTE.id
+-- SELECT name FROM Employee
+-- JOIN CTE
+-- ON Employee.id = CTE.id
+
+
+SELECT e1.name as name FROM Employee e1
+JOIN Employee e2
+ON e1.id = e2.managerId
+GROUP BY e2.managerId
+HAVING COUNT(e1.id) >= 5
