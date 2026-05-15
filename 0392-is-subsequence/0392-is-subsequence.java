8ad9cc76e3f3780ecaab1aab1b870ed573c66587
PhatTruong <phattruongspkt@gmail.com>
2026-05-15 14:40:28 +0700

    Time: 69 ms (2.77%), Space: 42.6 MB (79.59%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -5,21 +5,12 @@ class Solution {
         int i =0, j = 0;
         int n = s.length(), m = t.length();
         while(i < n && j < m){
-            // System.out.println(i + " " + j + " " + cS[i] + " " +  cT[i] + " " + n + " " + m);
             if(cS[i] == cT[j]){
-                i++; j++;
-            } else {
-                j++;
-            }
+                i++;
+            } 
+            j++;
         }
-        if(n == 0) return true;
-        if(i < n && j == m - 1){
-            return false;
-        }
-        else if(i == n  && j <= m) {
-            return true;
-        }
-        System.out.println(i + " " + j);
-        return false;
+        
+        return i == n;
     }
 }
