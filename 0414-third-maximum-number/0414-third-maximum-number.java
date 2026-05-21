903d03fb4dcd727f2105147f0b5026fe4ab7a53b
PhatTruong <phattruongspkt@gmail.com>
2026-05-21 10:22:31 +0700

    Time: 8 ms (18.31%), Space: 45.4 MB (30.9%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,43 +1,5 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        // int n = nums.length;
-        // List<Integer> ans = new ArrayList<>();
-        // for(int i: nums){
-        //     ans.add(i);
-        // }
-        // Collections.sort(ans, Collections.reverseOrder());
-        // // System.out.println(n);
-        // int count= 2;
-        // int i = 1;
-        // int maxF0 = ans.get(0);
-        // int maxF1 = Integer.MIN_VALUE;
-        // int maxF2 = Integer.MIN_VALUE;        
-        // if(n < 3) {
-        //     return ans.get(0);
-        // }
-        // else if((n == 3 && ans.get(2) == ans.get(1) )|| 
-        // (n >= 3 && ans.get(0) == ans.get(1))){
-        //     return ans.get(0);
-        // }
-        // else if (n == 3 && ans.get(1) > ans.get(2)){
-        //     return ans.get(2);
-        // }
-        //  else {
-        //     for(i = 1; i < n; i++){
-        //         if(ans.get(i) < maxF0 && maxF1 < ans.get(i)){
-        //             maxF1 = ans.get(i);
-        //         } 
-        //         if(maxF0 > maxF1 && ans.get(i) < maxF0 && ans.get(i) < maxF1  &&  ans.get(i) > maxF2){
-        //             maxF2 = ans.get(i);
-        //             break;
-        //         }
-        //     }
-        // }
-        // if(maxF2 == Integer.MIN_VALUE) {
-        //     return ans.get(0);
-        // } else {
-        //     return maxF2;
-        // }
         int n = nums.length;
         List<Integer> ans = new ArrayList<>();
         for(int i: nums){
