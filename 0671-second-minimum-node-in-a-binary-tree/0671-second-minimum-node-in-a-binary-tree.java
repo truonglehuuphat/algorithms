dd4892df2465f06d864f28c765ee72daddcfdb2b
PhatTruong <phattruongspkt@gmail.com>
2026-05-29 13:34:24 +0700

    Time: 0 ms (100%), Space: 42.2 MB (98.47%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
@@ -19,18 +19,13 @@ class Solution {
     private void travser(TreeNode root){
         if(root == null) return;
         minFirst = minFirst > root.val ? root.val: minFirst;
-
         if(minSec == -1) {
             minSec = minFirst;
-            // System.out.println("24");
         }
-        // System.out.println(minSec + " " + minFirst + " " + root.val + " " + (root.val > minSec ? "1" : "0") + " " + (minSec == minFirst ? "1" : "0"));
         if(minSec == minFirst && root.val > minSec){
             minSec = root.val;
-            // System.out.println("28" + " " + minSec);
         }
         if(minSec > root.val && root.val != minFirst){
-            // System.out.println("32");
             minSec = root.val;
         }
         travser(root.left);
