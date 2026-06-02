cf9da1cb161ad485150584ad56e71fb6e991d420
PhatTruong <phattruongspkt@gmail.com>
2026-06-02 10:00:25 +0700

    Time: 3 ms (66.67%), Space: 47.1 MB (23.41%) - LeetHub

---
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int res = Integer.MAX_VALUE;
+        int n = landStartTime.length;
+        int m = waterStartTime.length;
+        for(int i = 0 ; i < n;i++){
+            for(int j = 0; j <m ;j++){
+                int land = landStartTime[i] + landDuration[i];
+                int land_water = Math.max(land, waterStartTime[j]) + waterDuration[j];
+                res = Math.min(res, land_water);
+
+                int water = waterStartTime[j] + waterDuration[j];
+                int water_land = Math.max(water, landStartTime[i]) + landDuration[i];
+                res = Math.min(res,water_land);
+            }
+        }
+        return res;
+    }
+}
