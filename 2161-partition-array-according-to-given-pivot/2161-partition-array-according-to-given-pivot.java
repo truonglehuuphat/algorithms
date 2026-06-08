f6ac0993c33c4fb0a16790ff0f8e98448abefcd5
PhatTruong <phattruongspkt@gmail.com>
2026-06-08 10:52:30 +0700

    Time: 21 ms (5.1%), Space: 167.4 MB (5.16%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -2,26 +2,20 @@ class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         List<Integer> lessNum = new ArrayList<>();
         List<Integer> greaterNum = new ArrayList<>();
+        List<Integer> equalNum = new ArrayList<>();
         for(int d: nums){
             if(d < pivot){
                 lessNum.add(d);
             }
-            if (d > pivot) {
+            else if (d > pivot) {
                 greaterNum.add(d);
+            } else {
+                equalNum.add(d);
             }
         }
-        for(int d: nums){
-            if(d == pivot){
-                lessNum.add(d);
-            }
-        }
-        List<Integer> ans = new ArrayList<>();
-        for(int d : lessNum){
-            ans.add(d);
-        }
-        for(int d: greaterNum){
-            ans.add(d);
-        }
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        lessNum.addAll(equalNum);
+        lessNum.addAll(greaterNum);
+        // List<Integer> ans = new ArrayList<>();
+        return lessNum.stream().mapToInt(Integer::intValue).toArray();
     }
 }
