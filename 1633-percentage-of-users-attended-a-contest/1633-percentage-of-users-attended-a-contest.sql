a5c9f33d51f628b8b87fcf9578482472141dcb58
PhatTruong <phattruongspkt@gmail.com>
2026-06-12 13:59:19 +0700

    Time: 1176 ms (13.79%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
-
+WITH CTE as (
+    SELECT 
+    COUNT(user_id) as total_count
+    FROM Users
+)
 SELECT r.contest_id ,
-    ROUND(COUNT(r.contest_id)*100/( 
-        SELECT 
-        COUNT(user_id) as total_count
-        FROM Users),
-        2
-    ) as percentage 
+    ROUND(COUNT(r.contest_id)*100/CTE.total_count , 2) as percentage 
 FROM Users u
 JOIN Register r
 ON u.user_id = r.user_id
+CROSS JOIN CTE
 GROUP BY r.contest_id 
-ORDER BY percentage DESC, r.contest_id ASC
+ORDER BY percentage DESC, r.contest_id
