707b84f440631bf11842fe7d11dd5e7f72140d39
PhatTruong <phattruongspkt@gmail.com>
2026-06-16 11:00:06 +0700

    Time: 40 ms (10.29%), Space: 55.7 MB (20.59%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -5,18 +5,19 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             System.out.println(res + " " + s.charAt(i));
             int n = res.length();
-            if(s.charAt(i) == '*'){
+            char c = s.charAt(i);
+            if(c == '*'){
                 if(n > 0){
                     res.delete(n-1, n);
                 }
-            } else if(s.charAt(i) == '#'){
-                if(n > 0){
+            } else if(c == '#'){
+                // if(n > 0){
                     res.append(res);
-                }
-            } else if(s.charAt(i) == '%'){
+                // }
+            } else if(c == '%'){
                 res.reverse();
             } else {
-                res.append(s.charAt(i));
+                res.append(c);
             }
         }
         return res.toString();
