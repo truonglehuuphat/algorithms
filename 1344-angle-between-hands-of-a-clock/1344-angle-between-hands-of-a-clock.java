221ab4219671b4c06c5539abd1b4da4a674405c5
PhatTruong <phattruongspkt@gmail.com>
2026-06-18 13:43:02 +0700

    Time: 7 ms (2.56%), Space: 45.4 MB (12.45%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
@@ -5,7 +5,7 @@ class Solution {
         double hourAng = ((minutes / 12.0) * 6) +  ((hour % 12) * vachnho) * difAngle;
         double minuAng = minutes * difAngle;
         
-        // System.out.println(hourAng + " " + minuAng);
+        System.out.println(hourAng + " " + minuAng);
         if(minuAng > 180 ) {
             minuAng = 360 - minuAng;
             return Math.abs(hourAng + minuAng) > 180 ? Math.abs(360 - Math.abs(hourAng + minuAng)): Math.abs(hourAng + minuAng) ;
