f94e5ff48397f2d6787764591d4d69adbf2f704d
PhatTruong <phattruongspkt@gmail.com>
2026-06-25 09:15:12 +0700

    Time: 14 ms (70.32%), Space: 47.6 MB (99.86%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -16,14 +16,15 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int rows = mat.length;
         int cols = mat[0].length;
-        // boolean[][] visited = new boolean[rows][cols];
+        boolean[][] visited = new boolean[rows][cols];
         Queue<Pair> q = new ArrayDeque<>();
         for(int i = 0 ; i < rows; i++){
             for(int j = 0 ; j < cols; j++){
                 if(mat[i][j] == 0){
                     q.offer(new Pair(i,j));
+                    visited[i][j] = true;
                 } else {
-                    mat[i][j] = -1;
+                    // mat[i][j] = -1;
                 }
             }
         }
@@ -35,10 +36,11 @@ class Solution {
             for(int k = 0; k < 4; k++){
                 int new_i = i + row_b[k];
                 int new_j = j + col_b[k];
-                if(!valid(new_i, new_j, rows, cols) || mat[new_i][new_j] != -1) continue;
-                mat[new_i][new_j] = 1 + mat[i][j];
-                q.offer(new Pair(new_i, new_j));
-                // visited[new_i][new_j] = true;
+                if(valid(new_i, new_j, rows, cols) && visited[new_i][new_j] != true){
+                    mat[new_i][new_j] = 1 + mat[i][j];
+                    q.offer(new Pair(new_i, new_j));
+                    visited[new_i][new_j] = true;
+                }
             }
         }
         return mat;
