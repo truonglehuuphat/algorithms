bb2319129d7d0b4688eb2e63000737b352b969c9
PhatTruong <phattruongspkt@gmail.com>
2026-06-29 10:13:10 +0700

    Time: 128 ms (51.97%), Space: 258.9 MB (85.67%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -2,31 +2,27 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         int size = edges.length;
         if(size == 0) return true;
-        boolean[] visited = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
+
         HashMap<Integer, List<Integer>> graph = new HashMap<>();
         for(int[] e:edges){
             graph.computeIfAbsent(e[0], k-> new ArrayList<>()).add(e[1]);
             graph.computeIfAbsent(e[1], k-> new ArrayList<>()).add(e[0]);
         }
-        // for(int i = source; i < destination; i++ ){
-            q.offer(source);
-            visited[source] = true;
-            while(!q.isEmpty()){
-                int index = q.poll();
-                // System.out.println("index: "+ index);
-                // if(visited[index] == true) continue;
-                List<Integer> value = graph.get(index);
-                if(value == null) return false;
-                for(int d: value){
-                //    System.out.println("d: "+ d);
-                    if(visited[d] == false){
-                        q.offer(d);
-                        visited[d] = true;
-                    }
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();        
+        q.offer(source);
+        visited[source] = true;
+        while(!q.isEmpty()){
+            int index = q.poll();
+            List<Integer> value = graph.get(index);
+            if(index == destination) return true;
+            for(int d: value){
+                if(visited[d] == false){
+                    q.offer(d);
+                    visited[d] = true;
                 }
             }
-        // }
-        return visited[destination];
+        }
+        return false;
     }
 }
