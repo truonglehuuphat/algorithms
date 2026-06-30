0ac028149df7d90c69d6cd0aa719114b5af2f8f6
PhatTruong <phattruongspkt@gmail.com>
2026-06-30 13:22:36 +0700

    Time: 3 ms (10.6%), Space: 44.3 MB (39.98%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,62 @@
+class Solution {
+    class Pair {
+        int row;
+        int col;
+        Pair(int r, int c){
+            row = r;
+            col = c;
+        }
+    }
+    int[] row_b = {-1, 1, 0, 0};
+    int[] col_b = {0, 0, -1, 1};
+    Queue<Pair> q = new LinkedList<>();
+    private boolean isValid(int i , int n, int j, int m){
+        return i >= 0 && i < n && j >= 0 && j < m;
+    }
+    
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int freshOrange = 0;
+        int minutes = 0;
+        boolean[][] visited = new boolean[rows][cols];
+        for(int i = 0 ; i  < rows ; i++){
+            for(int j = 0 ; j < cols; j++){
+                if(grid[i][j] == 2){
+                   q.offer(new Pair(i,j));
+                }
+                else if(grid[i][j] == 1){
+                    freshOrange++;
+                } else {
+                    visited[i][j] = true;
+                }
+            }
+        }
+        if(freshOrange == 0 ) return 0;
+        if(q.size() == 0) return -1;
+        while(!q.isEmpty()){
+            int store = q.size();
+            // System.out.println("freshOrange " + freshOrange + " minutes "+ minutes);
+            if(freshOrange == 0) return minutes;
+            for(int z = 0; z < store; z++){
+                Pair node = q.poll();
+                int i = node.row;
+                int j = node.col;
+                for(int k = 0; k < 4; k++){
+                    int new_row = i + row_b[k];
+                    int new_col = j + col_b[k];
+                    if(isValid(new_row, rows, new_col, cols) && visited[new_row][new_col] == false){
+                        if(grid[new_row][new_col] == 1){
+                            grid[new_row][new_col] = 2;
+                            freshOrange--;
+                            visited[new_row][new_col] = true;
+                            q.offer(new Pair(new_row, new_col));
+                        }
+                    }
+                }          
+            }
+            minutes++;
+        }
+        return freshOrange == 0 ? minutes: -1;
+    }
+}
