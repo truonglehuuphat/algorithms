85903c031c4e77cddb77781b949b0c80e660af32
PhatTruong <phattruongspkt@gmail.com>
2026-07-10 09:47:32 +0700

    Time: 2 ms (43.43%), Space: 47 MB (87.82%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,22 +1,32 @@
 class Solution {
-    void dfs(int node, int[][] isConnected, boolean[] visit){
-        visit[node] = true;
-        for(int i = 0;i < isConnected.length; i++){
-            if(isConnected[node][i] ==1 && !visit[i]){
-                dfs(i,isConnected, visit);
-            }
-        }
-    }
+    // void dfs(int node, int[][] isConnected, boolean[] visit){
+    //     visit[node] = true;
+    //     for(int i = 0;i < isConnected.length; i++){
+    //         if(isConnected[node][i] ==1 && !visit[i]){
+    //             dfs(i,isConnected, visit);
+    //         }
+    //     }
+    // }
 
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         int numberOfProvinces = 0;
-        boolean[] visit = new boolean[n];
+        boolean[] visited = new boolean[n];
         for(int i = 0; i < n; i++){
-            if(!visit[i]){
-                numberOfProvinces++;
-                dfs(i, isConnected, visit);
+            if(visited[i]== true) continue;
+
+            Queue<Integer> q = new LinkedList<>();
+            q.offer(i);
+            while(!q.isEmpty()){
+                int node = q.poll();
+                for(int j = 0 ; j < n ; j++){
+                    if(visited[j]== false && isConnected[node][j] == 1){
+                        q.offer(j);
+                        visited[j] =  true;
+                    }
+                }
             }
+            numberOfProvinces++;
         }
         return numberOfProvinces;
     }
